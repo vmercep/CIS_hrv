64483e721f73e0e3be6c0091f273e2c3214f3137
vladimir <vladimir@DESKTOP-LS7S78A>
2021-11-22 22:51:16 +0100

    added try catch in app start

--- 385_fisk/Program.cs
+++ 385_fisk/Program.cs
@@ -1,6 +1,7 @@
 ﻿using _385_fisk.Exceptions;
 using AutoUpdaterDotNET;
 using System;
+using System.Diagnostics;
 using System.Management;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
@@ -13,21 +14,33 @@ internal static class Program {
     [STAThread]
     private static void Main () {
 
-        ManagementClass mngmtClass = new ManagementClass("Win32_Process");
-        foreach (ManagementObject o in mngmtClass.GetInstances())
+        try
         {
-            if (o["Name"].Equals("MerlinX2.exe"))
+            ManagementClass mngmtClass = new ManagementClass("Win32_Process");
+            foreach (ManagementObject o in mngmtClass.GetInstances())
             {
-                String commandLine = (String)o["CommandLine"];
-                Regex envRE = new Regex(@"/cis (.*)");
-                Match m = envRE.Match(commandLine);
-                if (m.Success)
+                if (o["Name"].Equals("MerlinX2.exe"))
                 {
-                    //Console.WriteLine(o["Name"] + " [" + m.Groups[1] + "]");
-                    Helper.Globals.Name = m.Groups[1].Value;
+                    String commandLine = (String)o["CommandLine"];
+                    Regex envRE = new Regex(@"/cis (.*)");
+                    Match m = envRE.Match(commandLine);
+                    if (m.Success)
+                    {
+                        //Console.WriteLine(o["Name"] + " [" + m.Groups[1] + "]");
+                        Helper.Globals.Name = m.Groups[1].Value;
+                    }
                 }
             }
+
+        }catch(Exception e)
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("Error ocured in trying to find merlin exe "+e.Message, EventLogEntryType.Error);
+            }
         }
+        
         Helper.LogFile.InitConfig();
         CentralniInformacijskiSustav.CreateDirectories();
         Application.EnableVisualStyles();
